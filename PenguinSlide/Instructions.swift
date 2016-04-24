c523e52a26a20549f4a3470cd8e81219b979369e
Yuliya <ylevitsk@aol.com>
2016-04-24 13:57:13 -0700

    Added files via upload

---
+++ PenguinSlide/Instructions.swift
@@ -0,0 +1,9 @@
+//
+//  Instructions.swift
+//  PenguinSlide
+//
+//  Created by Moe Wilson on 6/2/15.
+//  Copyright (c) 2015 Yuliya Levitskaya. All rights reserved.
+//
+
+import Foundation
---
+++ PenguinSlide/PlayViewController.swift
@@ -0,0 +1,11 @@
+//
+//  PlayViewController.swift
+//  PenguinSlide
+//
+//  Created by Moe Wilson on 5/5/15.
+//  Copyright (c) 2015 Yuliya Levitskaya. All rights reserved.
+//
+
+class PlayViewController: UIViewController {
+
+}
